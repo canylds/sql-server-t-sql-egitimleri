7fa7fa463a671e1c91b110d8b385e8ac8feb2ee3
canyo <canyoldas5410@gmail.com>
2025-02-20 13:59:14 +0300

    20-) T-SQL Inner Join'de İki Tabloyu Birleştirme dersi sorguları yazıldı

--- 20-) T-SQL Inner Join'de İki Tabloyu Birleştirme.sql
+++ 20-) T-SQL Inner Join'de İki Tabloyu Birleştirme.sql
@@ -3,4 +3,32 @@ USE KuzeyRuzgari
 -- Inner Join
 
 -- Genel Mantýk
--- SELECT * FROM
+-- SELECT * FROM Tablo1 INNER JOIN Tablo2 ON Tablo1.IliskiliKolon = Tablo2.IliskiliKolon
+
+-- Tablolara alias tanýmlanabilir.
+-- SELECT * FROM Tablo1 t1 INNER JOIN Tablo2 t2 ON t1.IliskiliKolon = t2.IliskiliKolon 
+
+-- Hangi personel hangi satýþlarý yapmýþtýr? (Personeller, Satislar)
+SELECT * FROM Personeller p INNER JOIN Satislar s ON p.PersonelID = s.PersonelID
+
+-- Hangi ürün hangi kategoride? (Urunler, Kategoriler)
+SELECT u.UrunAdi, k.KategoriAdi FROM Urunler u INNER JOIN Kategoriler k ON u.KategoriID = k.KategoriID
+
+-- Where Komutunun Kullanýmý
+
+
+-- Beverages kategorisindeki ürünlerim. (Urunler, Kategoriler)
+SELECT u.UrunAdi FROM Kategoriler k INNER JOIN Urunler u ON k.KategoriId = u.KategoriID WHERE k.KategoriAdi = 'Beverages'
+
+-- Beverages kategorisindeki ürünlerimin sayýsý kaçtýr? (Urunler, Kategoriler)
+SELECT COUNT(u.UrunAdi) FROM Kategoriler k INNER JOIN Urunler u ON k.KategoriID = u.KategoriID WHERE k.KategoriAdi = 'Beverages'
+
+-- Seafood kategorisindeki ürünlerin listesi (Urunler, Kategoriler)
+SELECT u.UrunAdi from Kategoriler k INNER JOIN Urunler u ON k.KategoriId = u.KategoriID WHERE k.KategoriAdi = 'Seafood'
+
+-- Hangi satýþý hangi çalýþaným yapmýþ? (Satislar, Personeller)
+SELECT s.SatisID, p.Adi + ' ' + p.Soyadi FROM Satislar s INNER JOIN Personeller p ON s.PersonelID = p.PersonelID
+
+-- Faks numarasý "null" olmayan tedarikçilerden alýnmýþ ürünler nelerdir? (Urunler, Tedarikciler)
+SELECT u.UrunAdi FROM Urunler u INNER JOIN Tedarikciler t ON u.TedarikciID = t.TedarikciID WHERE t.Faks <> 'Null'
+SELECT u.UrunAdi FROM Urunler u INNER JOIN Tedarikciler t ON u.TedarikciID = t.TedarikciID WHERE t.Faks IS NOT NULL
