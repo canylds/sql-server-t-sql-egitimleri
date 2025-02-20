f50945614cfa0b53aaa5e757d59217ca9e4021ed
canyo <canyoldas5410@gmail.com>
2025-02-20 15:31:55 +0300

    24-) T-SQL Outer Join(Left, Right, Full) İle Tabloları Birleştirme.sql eklendi

---
+++ 24-) T-SQL Outer Join(Left, Right, Full) İle Tabloları Birleştirme.sql
@@ -0,0 +1,16 @@
+USE KuzeyRuzgari
+
+-- Inner Join'de eþleþen kayýtlar getiriliyordu. Outer Join'de ise eþleþmeyen kayýtlarda getirilmektedir.
+
+-- Left Join
+-- Join ifadesinin solundaki tablodan tüm kayýtlarý getirir. Saðýndaki tablodan eþleþenleri yan yana, eþleþmeyenleri null olarak getirir.
+
+
+-- Right Join
+-- Joinin saðýndaki tablonun tamamnýný getirecek, solundakinden eþleþenleri ayný satýrda, eþleþmeyenleri null olarak getirecek.
+
+
+-- Full Join
+-- Joinin iki tarafýndaki tablolardan eþleþen eþleþmeyen hepsini getirir.
+
+-- https://www.youtube.com/watch?v=U-FEqpGdxpw videosunda kodlar var.
