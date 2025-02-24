0fe57d8e721071edfe93dc8555a27662406d6167
canyo <canyoldas5410@gmail.com>
2025-02-24 14:34:55 +0300

    34-) T-SQL With Cube Komutu.sql eklendi

---
+++ 34-) T-SQL With Cube Komutu.sql
@@ -0,0 +1,11 @@
+USE KuzeyRuzgari
+
+-- WITH CUBE
+-- GROUP BY ile gruplanmýþ veri kümesinde teker teker toplam alýnmasýný saðlar.
+
+SELECT SatisID, UrunID, SUM(Miktar) FROM [Satis Detaylari] GROUP BY SatisID, UrunID WITH CUBE
+
+SELECT KategoriID, UrunID, SUM(TedarikciID) FROM Urunler GROUP BY KategoriID, UrunID WITH CUBE
+
+-- [HAVING Þartýyla Beraber WITH CUBE]
+SELECT SatisID, UrunID, SUM(Miktar) FROM [Satis Detaylari] GROUP BY SatisID, UrunID WITH CUBE HAVING SUM(Miktar) > 100
